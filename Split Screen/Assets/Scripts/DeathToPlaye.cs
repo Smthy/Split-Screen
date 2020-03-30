b68649afc7b4cc106b42dac05e8caf600e58426d
Smthy <61051295+Smthy@users.noreply.github.com>
2020-03-30 15:41:57 +0100

    AI in game
    Ai and way to lose

---
+++ Split Screen/Assets/Scripts/DeathToPlaye.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathToPlaye : MonoBehaviour
+{
+    public void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Time.timeScale = 1f;
+            Destroy(gameObject);
+        }
+    }
+
+}
